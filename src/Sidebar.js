73221c7161f025d588451e69672733410032cd24
Eric <ericmiller@umn.edu>
2018-10-11 22:10:18 -0500

    refactored Sidebar.js as a pure function

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,53 +6,63 @@ import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
 import CheckIcon from '@material-ui/icons/Check'
 
-class Sidebar extends React.Component {
-  render() {
-    const { cities, searchTerm, updateSearch, selectedListItem, updateListSelection } = this.props
-    const searchRegex = RegExp(searchTerm,'i')
-    let searchCities
-    if (searchTerm) {
-      searchCities = cities.filter( city => searchRegex.test(city) )
-    } else {
-      searchCities = cities;
-    }
+const Item = ({ city, updateListSelection, selectedListItem }) => (
+  <ListItem 
+    divider
+    button
+    className='clickMe'
+    id={city} 
+    onClick={ (event) => { 
+      updateListSelection(event.target.id)
+    }}
+    selected={ selectedListItem === city }
+  >
+    <ListItemText primary={city} />
+    <ListItemIcon id={city}>
+      <CheckIcon 
+        style={{color: selectedListItem === city ? 'green' : '#000000'} }
+      />
+    </ListItemIcon> 
+  </ListItem>             
+)
 
-    return (
-      <div className="Sidebar">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Eric's European Adventures</h1>
-        </header>
-        <form>
-          <input type="search" name="search" placeholder="Search for Places" 
-            value={searchTerm || ''} 
-            onChange={ (event) => updateSearch(event.target.value)} 
-          />  
-        </form> 
-        <div className="sidebarList">
-          <List component="nav">
-            {searchCities.map( city => {
-              return <ListItem divider button className='clickMe' 
-                  key={city} id={city} 
-                  onClick={ (event) => { 
-                    updateListSelection(event.target.id)
-                  }}
-                  selected={ selectedListItem === city }
-                  >
-                <ListItemText primary={city} />
-                <ListItemIcon id={city}>
-                  <CheckIcon 
-                    style={{color: selectedListItem === city ? 'green' : '#000000'} }
-                  />
-                </ListItemIcon> 
-              </ListItem>
-            })} 
-          </List>
-        </div>
-        <div className='footer'></div>
+const Sidebar = ({ cities, searchTerm, updateSearch, selectedListItem, updateListSelection }) => {
+  const searchRegex = RegExp(searchTerm,'i')
+    const searchCities = (() => {
+      if (searchTerm) {
+      return cities.filter( city => searchRegex.test(city) )
+    } else {
+      return cities;
+    }
+    })();
+
+  return (
+    <div className="Sidebar">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Eric's European Adventures</h1>
+      </header>
+      <form>
+        <input type="search" name="search" placeholder="Search for Places" 
+          value={searchTerm || ''} 
+          onChange={ (event) => updateSearch(event.target.value)} 
+        />  
+      </form> 
+      <div className="sidebarList">
+        <List component="nav">
+          {searchCities.map( city => 
+            <Item
+              city={city}
+              updateListSelection={updateListSelection}
+              selectedListItem={selectedListItem}
+              key={city} 
+            />
+          )} 
+        </List>
       </div>
-    );
-  }
+      <div className='footer'></div>
+    </div>
+  );
 }
 
 export default Sidebar;
